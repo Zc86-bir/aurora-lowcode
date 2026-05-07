98ee570e0f2688135cabf9bc84ddb4725267a80f
Zc86-bir <dengyyh@gmail.com>
2026-05-07 15:01:47 +0800

    fix: register missing VueQueryPlugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import 'uno.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import router from './router'
 import i18n from './i18n'
 import App from './App.vue'
@@ -9,6 +10,7 @@ import './plugins/api-interceptor'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueQueryPlugin)
 app.use(router)
 app.use(i18n)
 
